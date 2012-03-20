185fdcfd8f6cbf7a9fed1f64403f0a855bb772a5
Rafael Romero <rafaelgromero@gmail.com>
2012-03-20 17:21:13 -0400

    Ha! I just realized I never added this file; if this broke your code, try again

---
+++ models/Board/Globals.js
@@ -0,0 +1,21 @@
+/**
+ * Globals.js
+ *
+ * JS Module that contains all globals and constants shared by all modules
+ */
+
+// Hex Types
+
+HexTypeEnum = {
+    INACTIVE : "Inactive",
+    ACTIVE : "Active",
+
+    WOOD: "Wood",
+    SHEEP: "Sheep",
+    WHEAT: "Wheat",
+    STONE: "Stone",
+    BRICK: "Brick",
+
+    DESERT: "Desert",
+    WATER: "Water",
+};
