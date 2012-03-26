511aedf3412048d8fcd1b3c0ecb7e02092253ff1
Christopher Triolo <ctriolo@princeton.edu>
2012-03-25 20:06:31 -0400

    moved Board rendering logic into UIBoard.

--- controllers/board.js
+++ controllers/board.js
@@ -1,156 +1,39 @@
 /**
- * newBoard.js
+ * Board.js
  *
  * Controller for board games
  */
 
 // Dependencies
-
-var Hex = require('../models/Board/Hex')
-  , Board = require('../models/Board/Board');
+var Board = require('../models/Board/Board')
+  , UIBoard = require('../models/Board/UIBoard');
 
 // Global Memory
 var boards = {}; // will be moved into BoardProvider
 
 // Constants
-var INT_DIRECTIONS = ["W", "NW", "NE", "E", "SE", "SW"];
-var EDGE_DIRECTIONS = ["NW", "N", "NE", "SE", "S", "SW"];
 var MIN = 3;
 var MAX = 5;
-var SVG_WIDTH = 800;
-var SVG_HEIGHT = 800;
-
-/**
- * addTile
- *
- * Add the unique elements associated with a tile to `hexes`, `edges`,
- * and `vertices` to help build a board.
- */
-function addTile(hexEdgeLength, originX, originY, x, y, hex, hexes, edges, vertices) {
-
-  // HEX COORDINATES
-
-  var C = hexEdgeLength
-    , A = .5 * C
-    , B = .866 * C;
-
-  // Build a simple hex
-  var points = [
-    {'x': 0,   'y': B},
-    {'x': A,   'y': 0},
-    {'x': A+C, 'y': 0},
-    {'x': 2*C, 'y': B},
-    {'x': A+C, 'y': 2*B},
-    {'x': A,   'y': 2*B}
-  ];
-
-  // Find the top left corner of the containing square
-  init_x = originX + x * (A + C);
-  init_y = originY + y * 2 * B;
-
-  if (Math.abs(x) % 2 == 1) {
-    if ((MAX-MIN) % 2 == 1)
-        init_y -= B;
-    else
-        init_y += B;
-  }
-  // deal with starting on the "bottom" of the rectangle
-  if ((MAX-MIN)%2 == 1)
-    init_y += B;
-
-  // Add starting points
-  for (var i = 0; i < 6; i++) {
-    points[i].x += init_x;
-    points[i].y += init_y;
-  }
-
-  // Find the hex's center
-  center = {
-    'x': init_x + A + C/2,
-    'y': init_y + B,
-  };
-
-  // ADD VERTICES, EDGES, AND HEX
-
-  for (var i = 0; i < 6; i++) {
-    // Add the vertex
-    var vIndex = hex.intersections[INT_DIRECTIONS[i]];
-    vertices[vIndex] = {
-      'index': vIndex,
-      'x': points[i].x,
-      'y': points[i].y,
-    };
-
-    // Add the edge
-    var eIndex = hex.edges[EDGE_DIRECTIONS[i]];
-    edges[hex.edges[EDGE_DIRECTIONS[i]]] = {
-      'index': eIndex,
-      'x0': points[i].x,
-      'y0': points[i].y,
-      'x1': points[(i+1)%6].x,
-      'y1': points[(i+1)%6].y,
-      'port': "None",
-    };
-  }
-
-  // Add the hex
-  hexes[hex.index] = {
-    'points': points,
-    'center': center,
-    'type': hex.type ? hex.type : 'Sea',
-    'number': hex.number,
-    'radius': hexEdgeLength,
-    'index': hex.index,
-  };
-}
 
 /**
  * view
  *
- * Renders a rectangular board.
+ * Renders a game, including a board and players
  */
 module.exports.view = function(req, res) {
   var id = req.params.id?req.params.id:'default';
   if (id in boards) {
     // Fetch an existing board
-    var b = boards[id];
+    var board = boards[id];
   } else {
     // Create a new board
-    var b = new Board(MIN, MAX);
-    boards[id] = b;
+    var board = new Board(MIN, MAX);
+    boards[id] = board;
   }
 
-  // Everything below renders a board. Should be put into a UIBoard object.
-
-  var board = b.json2();
-  var hexes = [];
-  var vertices = [];
-  var edges = [];
-  var hexEdgeLength = Math.min((SVG_HEIGHT)/(board.gridHeight+1)/(2*Math.sin(Math.PI/3)), SVG_WIDTH/(board.gridWidth+2)/1.5);
-  var originX = (SVG_WIDTH)/2 - hexEdgeLength - (MAX -MIN)*1.5*hexEdgeLength;
-  var originY = 40;
-
-  // Foreground Hexes
-  for (var i = 0; i < board.hexes.length; i++) {
-    fore_hex = board.hexes[i];
-    if (fore_hex.type !== "Inactive") {
-      addTile(hexEdgeLength, originX, originY, fore_hex.grid.x, fore_hex.grid.y, fore_hex, hexes, edges, vertices);
-    }
-  }
-
-  // Add ports, TODO: get this in addTile
-  for (var i = 0; i < board.edges.length; i++) {
-    edge = board.edges[i];
-    if (edge.isActive && edge.port) {
-      edges[edge.index].port = edge.port;
-    }
-  }
-
-  // TO CLEAN UP THE ARRAYS PASSED TO THE VIEW RENDERER. TODO: HAVE BOARD ONLY INDEX AND GIVE US ACTIVE ELEMENTS
-  var temp;
-  temp = []; for (var i = 0; i < hexes.length; i++) if (hexes[i]) temp.push(hexes[i]); hexes = temp;
-  temp = []; for (var i = 0; i < edges.length; i++) if (edges[i]) temp.push(edges[i]); edges = temp;
-  temp = []; for (var i = 0; i < vertices.length; i++) if (vertices[i]) temp.push(vertices[i]); vertices = temp;
-
-  res.render('board', {'layout':false, 'title':'Settle','hexes':hexes,'vertices':vertices,'edges':edges});
+  res.render('board', {
+    'layout': false,
+    'title': 'Settle',
+    'board': (new UIBoard(board)).render()
+  });
 };
---
+++ models/Board/UIBoard.js
@@ -0,0 +1,184 @@
+/**
+ * UIBoard
+ *
+ * Renders Board objects
+ *
+ * EXAMPLE:
+ *   var b = new Board(3,5);
+ *   var uib = new UIBoard(b);
+ *   var rendered = uib.render();
+ */
+
+// Constants
+var INT_DIRECTIONS = ["W", "NW", "NE", "E", "SE", "SW"];
+var EDGE_DIRECTIONS = ["NW", "N", "NE", "SE", "S", "SW"];
+var SVG_WIDTH = 800;
+var SVG_HEIGHT = 800;
+
+/**
+ * UIBoard
+ *
+ * @param   object  the Board object you wish to render
+ * @return  object  new UIBoard object
+ */
+function UIBoard(board) {
+  this.board = board.json2();
+  this.board.max = board.max;
+  this.board.min = board.min;
+  var hexEdgeLength = Math.min((SVG_HEIGHT)/(this.board.gridHeight+1)/(2*Math.sin(Math.PI/3)), SVG_WIDTH/(this.board.gridWidth+2)/1.5);
+  this.originX = (SVG_WIDTH)/2 - hexEdgeLength - (this.board.max - this.board.min) * 1.5 * hexEdgeLength;
+  this.originY = 40;
+  this.C = hexEdgeLength
+  this.A = .5 * hexEdgeLength
+  this.B = .866 * hexEdgeLength;
+}
+
+/**
+ * _getHexCenter (private)
+ *
+ * Gets the translation from (0,0) of the hex at [x,y].
+ *
+ * @param   x  int     the x coord of the hex
+ * @param   y  int     the y coord of the hex
+ * @return     object  in the form of {'x': x, 'y': y}
+ */
+UIBoard.prototype._getHexTranslation = function(x, y) {
+  var A = this.A, B = this.B, C = this.C;
+
+  // Find the top left corner of the containing square
+  var Tx = this.originX + x * (A + C);
+  var Ty = this.originY + y * 2 * B;
+
+  if (Math.abs(x) % 2 == 1) {
+    if ((this.board.max-this.board.min) % 2 == 1) Ty -= B;
+    else Ty += B;
+  }
+  // deal with starting on the "bottom" of the rectangle
+  if ((this.board.max-this.board.min)%2 == 1) Ty += B;
+
+  return {'x': Tx, 'y': Ty}
+};
+
+
+/**
+ * _getHexCoordinates (private)
+ *
+ * Get all the points/corners of the hex at [x,y].
+ *
+ * @param   x  int    the x coord of the hex
+ * @param   y  int    the y coord of the hex
+ * @return     array  in the form of [{'x': x, 'y': y}, ...]
+ */
+UIBoard.prototype._getHexCoordinates = function(x, y) {
+  var A = this.A, B = this.B, C = this.C;
+  var T = this._getHexTranslation(x, y);
+  return [
+    {'x': T.x + 0,   'y': T.y + B},
+    {'x': T.x + A,   'y': T.y + 0},
+    {'x': T.x + A+C, 'y': T.y + 0},
+    {'x': T.x + 2*C, 'y': T.y + B},
+    {'x': T.x + A+C, 'y': T.y + 2*B},
+    {'x': T.x + A,   'y': T.y + 2*B}
+  ];
+};
+
+/**
+ * _getHexCenter (private)
+ *
+ * Gets the center of the hex at [x,y].
+ *
+ * @param   x  int     the x coord of the hex
+ * @param   y  int     the y coord of the hex
+ * @return     object  in the form of {'x': x, 'y': y}
+ */
+UIBoard.prototype._getHexCenter = function(x, y) {
+  var A=this.A, B=this.B, C = this.C;
+  var T = this._getHexTranslation(x, y);
+  return {
+    'x': T.x + A + C/2,
+    'y': T.y + B,
+  };
+};
+
+/**
+ * _addTile (private)
+ *
+ * Add the unique elements associated with a tile to the `hexes`, `edges`,
+ * and `intersections` arrays to help build a board.
+ *
+ * @param  hex            object  the hex object from this.board.hexes[i]
+ * @param  hexes          array   the array to place rendered hexes in
+ * @param  edges          array   the array to place rendered edges in
+ * @param  intersections  array   the array to place rendered intersections in
+ */
+UIBoard.prototype._addTile = function(hex, hexes, edges, intersections) {
+  if (hex.type === "Inactive") return;
+
+  var points = this._getHexCoordinates(hex.grid.x, hex.grid.y);
+  var center = this._getHexCenter(hex.grid.x, hex.grid.y);
+
+  // Add the hex's surrounding paths and intersections
+  for (var i = 0; i < 6; i++) {
+    // Add the vertex
+    var iIndex = hex.intersections[INT_DIRECTIONS[i]];
+    intersections[iIndex] = {
+      'index': iIndex,
+      'x': points[i].x,
+      'y': points[i].y,
+    };
+
+    // Add the edge
+    var eIndex = hex.edges[EDGE_DIRECTIONS[i]];
+    edges[eIndex] = {
+      'index': eIndex,
+      'x0': points[i].x,
+      'y0': points[i].y,
+      'x1': points[(i+1)%6].x,
+      'y1': points[(i+1)%6].y,
+      'port': "None",
+    };
+    if (this.board.edges[eIndex].port) edges[eIndex].port = this.board.edges[eIndex].port;
+  }
+
+  // Add the hex
+  hexes[hex.index] = {
+    'points': points,
+    'center': center,
+    'type': hex.type ? hex.type : 'Sea',
+    'number': hex.number,
+    'radius': this.C,
+    'index': hex.index,
+  };
+}
+
+
+/**
+ * render
+ *
+ * Renders and returns the board objects to be drawn by board.jade
+ *
+ * @return  object  in the form of {'hexes': [...], 'edges': [...], 'intersections': [...]}
+ */
+UIBoard.prototype.render = function() {
+  var hexes = [];
+  var intersections = [];
+  var edges = [];
+
+  // Go through every hex and add it and its related elements to our arrays
+  for (var i = 0; i < this.board.hexes.length; i++)
+    this._addTile(this.board.hexes[i], hexes, edges, intersections);
+
+  // TODO: HAVE BOARD ONLY INDEX AND GIVE US ACTIVE ELEMENTS
+  var temp; // used to clean up `hexes`, `edges`, and `intersections`.
+  temp = []; for (var i = 0; i < hexes.length; i++) if (hexes[i]) temp.push(hexes[i]); hexes = temp;
+  temp = []; for (var i = 0; i < edges.length; i++) if (edges[i]) temp.push(edges[i]); edges = temp;
+  temp = []; for (var i = 0; i < intersections.length; i++) if (intersections[i]) temp.push(intersections[i]); intersections = temp;
+
+  return {
+    'hexes': hexes,
+    'edges': edges,
+    'intersections': intersections,
+  }
+};
+
+module.exports = UIBoard;
