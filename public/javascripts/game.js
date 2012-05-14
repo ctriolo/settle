bd4440903fc322b750fb2f3933bfc06954045fb1
Christopher Triolo <ctriolo@princeton.edu>
2012-05-13 21:47:46 -0400

    turn off redirect on disconnect, server shouldnt crash anymore anyway

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -554,8 +554,8 @@ console.log('(8). Player no. and replacing tag #VIDEO'+playerNo);
   });
 
   socket.on('disconnect', function() {
-    window.onbeforeunload = function() {};
-    window.location = 'http://www.cs.princeton.edu/~ctriolo/333.html';
+    //window.onbeforeunload = function() {};
+    //window.location = 'http://www.cs.princeton.edu/~ctriolo/333.html';
   });
 
   socket.on('updatePopup', function(playerCards) {
